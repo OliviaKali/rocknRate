ebe9a9e5c1aa3a955e802917104f0b78a27e2a52
OKali <o.kalinowski27@gmail.com>
2019-08-12 12:24:57 -0400

    Added html, css, and js files

---
+++ public/js/app.js
@@ -0,0 +1,81 @@
+80 lines (70 sloc)  2.15 KB
+    
+$(document).ready(function() {
+  var searchInput = $.getUrlVar('s');
+  if (null !== searchInput) {
+    var decodedSearch = decodeURIComponent(searchInput)
+    searchArtist(decodedSearch);
+  }
+
+  //executed on first page and searchbar on second page
+  $("#userSearch").on("click", function() {
+    event.preventDefault();
+    var userInput = $("#userInput")
+      .val()
+      .trim();
+var url = "/artist?s=" + encodeURIComponent(userInput);
+      $(location).attr('href',url);
+    // searchArtist(userInput);
+  });
+
+  $("#userSearchNav").on("click", function() {
+    event.preventDefault();
+    var userInput = $("#userInput")
+      .val()
+      .trim();
+
+    searchArtist(userInput);
+  });
+});
+
+function searchArtist(userInput) {
+  console.log(userInput)
+  $.ajax({
+    method: "POST",
+    url: "/api/search/",
+    data: {"artist": userInput}
+  }).then(function(response) {
+    console.log(response)
+    
+    $("#artistPlayerDiv").html(
+      `<div class="tile is-ancestor">
+          <div class="tile is-vertical is-8">
+            <div class="tile">
+              <div class="tile is-parent is-vertical">
+                <article class="tile is-child box">
+                  <p class="title">Spotify</p>
+                  <p class="subtitle">Top Left</p>
+                  <iframe src="https://open.spotify.com/embed/artist/${response.id}" width="300"
+                      height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                  </article>
+                 
+      </div>`
+    );
+
+    $("#artistImageDiv").html(
+              `<article class="tile is-child box">
+              <p class="title ">${response.name}</p>
+              <img id="artistImage" class="imageSize" src="${response.image}" />
+        </figure>
+      </article>`
+    );
+
+  });
+}
+
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});
