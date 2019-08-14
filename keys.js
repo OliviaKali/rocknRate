a42884a6f98293f45b70db0ec81a45d8debe09b6
OKali <o.kalinowski27@gmail.com>
2019-08-14 15:03:08 -0400

    spotify keys

--- keys.js
+++ keys.js
@@ -1,10 +1,9 @@
-// exports.spotify = {
-//     id: process.env.SPOTIFY_ID,
-//     secret: process.env.SPOTIFY_SECRET
-//   };
-
-  exports.spotify = {
-  id: 'd50ce48e78f34aa39c48d92fb71b19d1',
-  secret: 'e9aa1cb7fb41429da0a797966b92fbff'
-
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
   };
+
+  // exports.spotify = {
+  // id: 'd50ce48e78f34aa39c48d92fb71b19d1',
+  // secret: 'e9aa1cb7fb41429da0a797966b92fbff'
+  // };
